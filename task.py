8d8c0f81410d112a70bc1c3c00be0a0e492db112
Donald Taylor <taylordo@engr.orst.edu>
2021-05-20 17:09:24 -0600

    Added helper functions to simplify. Hopefully this will pass the flake8 complexity rules

--- task.py
+++ task.py
@@ -1,10 +1,92 @@
+ALL_DIGITS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+UPPER_CASE_HEX_ALPHAS = ['A', 'B', 'C', 'D', 'E', 'F']
+LOWER_CASE_HEX_ALPHAS = ['a', 'b', 'c', 'd', 'e', 'f']
+
+
+def is_hex(num_str):
+    if num_str[0:2].lower() == '0x':
+        return True
+    else:
+        return False
+
+
+def is_float(num_str):
+    for char in num_str:
+        if char == '.':
+            return True
+    return False
+
+
+def process_int(num_str):
+    number = 0
+    place = 1
+
+    for char in reversed(num_str):
+        # Check for non-numeric character
+        if char not in ALL_DIGITS:
+            return None
+        digit = ord(char) - 48
+        number += digit * place
+        place *= 10
+
+    return number
+
+
+def process_float(num_str):
+    num_str = num_str.split('.')
+
+    # Check for multiple decimal points
+    if len(num_str) > 2:
+        return None
+
+    before_decimal = num_str[0]
+    after_decimal = num_str[1]
+
+    number = 0.
+    place = 1
+
+    for char in reversed(before_decimal):
+        # Check for non-numeric character
+        if char not in ALL_DIGITS:
+            return None
+
+        digit = ord(char) - 48
+        number += digit * place
+        place *= 10
+
+    place = .1
+    for char in after_decimal:
+        # Check for non-numeric character
+        if char not in ALL_DIGITS:
+            return None
+
+        digit = ord(char) - 48
+        number += digit * place
+        place *= .1
+
+    return number
+
+
+def process_hex(num_str):
+    number = 0
+    place = 1
+    for char in reversed(num_str):
+        if char in ALL_DIGITS:
+            digit = ord(char) - 48
+        elif char in UPPER_CASE_HEX_ALPHAS:
+            digit = ord(char) - 55
+        elif char in LOWER_CASE_HEX_ALPHAS:
+            digit = - 87
+        else:
+            return None
+
+        number += digit * place
+        place *= 16
+
+    return number
+
+
 def conv_num(num_str):
-
-    ALL_DIGITS = ['1','2','3','4','5','6','7','8','9','0']
-    UPPER_CASE_HEX_ALPHAS = ['A','B','C','D','E','F']
-    LOWER_CASE_HEX_ALPHAS = ['a','b','c','d','e','f']
-
-    num_type = None
     positive = True
     number = None
 
@@ -21,88 +103,24 @@ def conv_num(num_str):
         positive = False
         num_str = num_str[1:]
 
-    # Determine if Hex, if hex strip off 0x
-    if num_str[0:2].lower() == '0x':
+    # Determine num_type, if hex strip off 0x
+    if is_hex(num_str):
         num_type = 'HEX'
         num_str = num_str[2:]
+    elif is_float(num_str):
+        num_type = 'FLOAT'
     else:
-        # Determine if Float
-        for char in num_str:
-            if char == '.':
-                num_type = 'FLOAT'
-
-    # Otherwise its an Int
-    if num_type is None:
         num_type = 'INT'
 
-    # Processing for INT
+    # process number based on type
     if num_type == 'INT':
-        number = 0
-        place = 1
-        for char in reversed(num_str):
-            # Check for non-numeric character
-            if char not in ALL_DIGITS:
-                return None
-
-            digit = ord(char) - 48
-            number += digit * place
-            place *= 10
-
-    # Processing for FLOAT
-    if num_type == 'FLOAT':
-        num_str = num_str.split('.')
-
-        # Check for multiple decimal points
-        if len(num_str) > 2:
-            return None
-
-        before_decimal = num_str[0]
-        after_decimal = num_str[1]
-
-        number = 0.
-        place = 1
-
-        for char in reversed(before_decimal):
-            # Check for non-numeric character
-            if char not in ALL_DIGITS:
-                return None
-
-            digit = ord(char) - 48
-            number += digit * place
-            place *= 10
-
-        place = .1
-        for char in after_decimal:
-
-            # Check for non-numeric character
-            if char not in ALL_DIGITS:
-                return None
-
-            digit = ord(char) - 48
-            number += digit * place
-            place *= .1
-
-    # Processing for HEX
-    if num_type == 'HEX':
-        number = 0
-        place = 1
-        for char in reversed(num_str):
-            if char in ALL_DIGITS:
-                digit = ord(char) - 48
-            elif char in UPPER_CASE_HEX_ALPHAS:
-                digit = ord(char) - 55
-            elif char in LOWER_CASE_HEX_ALPHAS:
-                digit = - 87
-            else:
-                return None
-
-            number += digit * place
-            place *= 16
+        number = process_int(num_str)
+    elif num_type == 'FLOAT':
+        number = process_float(num_str)
+    elif num_type == 'HEX':
+        number = process_hex(num_str)
 
     if positive:
         return number
     else:
         return -number
-
-
-
