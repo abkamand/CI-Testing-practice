3a09cb44ff58ab3f8c0dd8d49d424118daae8c83
Andrew Kamand <andrewkamand@gmail.com>
2021-05-22 11:01:25 -0700

    Fixed many logic errors, notably leap year check and month exit condition, deleted some old no longer used variables.

--- task.py
+++ task.py
@@ -1,83 +1,70 @@
 def my_datetime(num_sec):
-    # initialize start_day, start_month, start_year
-    start_day = 1
-    start_month = 1
-    start_year = 1970
-
     # convert seconds to days, 86400 seconds in a day
     days = num_sec // 86400
 
     # create array that represents the amount of days in a month in a yearly calendar
-    days_in_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    # initialize month_counter and year_counter to 0
-    month_counter = 0
-    year_counter = 0
-    # initialize leap_year_counter to 2
+    days_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # initialize leap_year_counter to 2 since we start at 1970
     leap_year_counter = 2
     # initialize normal_year days to 365 and leap_year_days to 366
     normal_year_days = 365
     leap_year_days = 366
 
-    # convert days to years, store in year_counter
-    while (days > 365):
-        if leap_year_counter == 4:
+    #initialize current_year to 1970, current_month to 1
+    current_year = 1970
+    current_month = 1
+
+    # convert days to years, update current_year
+    while (leap_year_counter == 4 and days > 366) or (leap_year_counter != 4 and days > 365):
+        # if we're in a leap year
+        if (current_year % 4 == 0 and current_year % 100 != 0) or (current_year % 400 == 0):
             days = days - leap_year_days
-            year_counter += 1
-            leap_year_counter = 0
+            leap_year_counter = 1
+        # not a leap year
         else:
             days = days - normal_year_days
-            year_counter += 1
             leap_year_counter += 1
+        
+        current_year +=1
     
     # testing output
-    print("Years to add =" + str(year_counter))
-
-    # convert leftover days to months, store in month_counter
-    #while (days < 365 and days > 30):
+    #print("Current Year =" + str(current_year))
     
-    # if we landed on a leap year, set 2nd element in days_in_months to 29
-    if leap_year_counter == 4:
-        days_in_months[1] = 29
+    # if we landed on a leap year, set 2nd element in days_months to 29
+    if (current_year % 4 == 0 and current_year % 100 != 0) or (current_year % 400 == 0):
+        days_months[1] = 29
 
-    if days > 30:
-        for month in days_in_months:
+    for month in days_months:
+        # break loop if we hit last month
+        if days <= month:
+            break
+        else:
             days = days - month
-            month_counter += 1 
+            current_month += 1
 
-            # break loop if we hit last month
-            if days < 31:
-                break
-    
-    # testing output
-    print("Months to add =" + str(month_counter))
-    
-    
-    # we now have our years to add, our months to add, all that remain are days
-    # add our counters to our start variables for days, months, years
-    final_day = start_day + days
-    final_month = start_month + month_counter
-    final_year = start_year + year_counter
+    # finally, get our current_day
+    current_day = days + 1
 
     # testing output
-    print("Unformatted Date =" + str(final_month) + "-" + str(final_day) + "-" + str(final_year))
+    #print("Current Month =" + str(current_month))
+
+    # testing output
+    #print("Unformatted Date =" + str(current_month) + "-" + str(days) + "-" + str(current_year))
 
     # convert our integers into MM-DD-YYYY string format
     string_day = ""
     string_month = ""
-    string_year = str(final_year)
+    string_year = str(current_year)
 
-    if final_day > 1 and final_day < 10:
-        string_day = "0" + str(final_day)
+    if current_day > 1 and current_day < 10:
+        string_day = "0" + str(current_day)
     else:
-        string_day = str(final_day)
+        string_day = str(current_day)
     
-    if final_month > 1 and final_month < 10:
-        string_month = "0" + str(final_month)
+    if current_month > 1 and current_month < 10:
+        string_month = "0" + str(current_month)
     else:
-        string_month = str(final_month)
+        string_month = str(current_month)
     
     return "Final, formatted date: " + string_month + "-" + string_day + "-" + string_year 
-
-# basic input testing
-#print(my_datetime(0))
-print(my_datetime(123456789))
